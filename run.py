145022f8a836b4f8f58565592c8e5ddcc1c48409
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-06-01 19:59:03 -0300

    deploy

---
+++ run.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask("__name__")
+app.config['SECRET_KEY'] = 'segredo'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route("/")
+def main():
+    return render_template("index.html")
+
+@app.route("/cube")
+def cube():
+    return render_template("cube.html")
+
+@socketio.on('connect')
+def on_connect():
+    print('Usuário conectado')
+
+@socketio.on('disconnect')
+def on_disconnect():
+    print('Usuário desconectou')
+
+@socketio.on('player_move')
+def on_player_move(data):
+    print('Movimento do player:', data)
+    emit('update_player', data, broadcast=True, include_self=False)
+
+if __name__ == "__main__":
+    import eventlet
+    import eventlet.wsgi
+    import sys
+
+    # só para rodar localmente com eventlet (prod: usar um servidor apropriado)
+    socketio.run(app, host="0.0.0.0", port=5000)
