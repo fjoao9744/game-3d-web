8aaacc4cfa737f8ae31cef00c79fefa15d34f238
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-06-02 18:49:09 -0300

    deploy

--- app.py
+++ app.py
@@ -29,7 +29,3 @@ def main():
 
 # port = int(os.environ.get("PORT", 5000))
 # socketio.run(app, host="0.0.0.0", port=port)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
