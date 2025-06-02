05a7619ceca194eb092926d06e50012e406c882a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2025-06-02 18:36:40 -0300

    deploy

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
+# from flask_socketio import SocketIO, emit
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'segredo'
-socketio = SocketIO(app, cors_allowed_origins="*")
+# app.config['SECRET_KEY'] = 'segredo'
+# socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route("/")
 def main():
     return render_template("index.html")
 
-@app.route("/cube")
-def cube():
-    return render_template("cube.html")
+# @app.route("/cube")
+# def cube():
+#     return render_template("cube.html")
 
-@socketio.on('connect')
-def on_connect():
-    print('Usuário conectado')
+# @socketio.on('connect')
+# def on_connect():
+#     print('Usuário conectado')
 
-@socketio.on('disconnect')
-def on_disconnect():
-    print('Usuário desconectou')
+# @socketio.on('disconnect')
+# def on_disconnect():
+#     print('Usuário desconectou')
 
-@socketio.on('player_move')
-def on_player_move(data):
-    print('Movimento do player:', data)
-    emit('update_player', data, broadcast=True, include_self=False)
+# @socketio.on('player_move')
+# def on_player_move(data):
+#     print('Movimento do player:', data)
+#     emit('update_player', data, broadcast=True, include_self=False)
 
-port = int(os.environ.get("PORT", 5000))
-socketio.run(app, host="0.0.0.0", port=port)
+# port = int(os.environ.get("PORT", 5000))
+# socketio.run(app, host="0.0.0.0", port=port)
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
